05d20ea8e1615b5f1ad1e6beffa309364bd7c781
Prathamesh Kulkarni <kulkarni@cytocybernetics.com>
2025-03-12 10:54:05 -0400

    Exercise 2.4 solution.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -43,34 +43,58 @@ const Total = ({ parts }) => {
   )
 }
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }, 
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course name={course.name} parts={course.parts} />;
+  return (
+    <>
+    {courses.map(course => 
+      <Course id={course.parts.id} name={course.name} parts={course.parts}/>
+    )}
+    </>
+  )
 }
 
 export default App;
