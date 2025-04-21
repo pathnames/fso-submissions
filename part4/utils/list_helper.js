06147bd7260f817d84a3c228abd89899439fbe22
pathnames <pk58@buffalo.edu>
2025-04-21 19:36:42 -0400

    Exercise 4.5 solution.

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -5,7 +5,15 @@ const dummy = (blogs) => {
 const totalLikes = blogs => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => {
+        return blog.likes > max.likes ? blog : max;
+    })
+}
+
 module.exports = {
-    dummy, 
-    totalLikes
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
