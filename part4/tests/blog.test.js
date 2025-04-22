89a38b7fdb4c728909a68b2470c6522d41cfa7c4
pathnames <pk58@buffalo.edu>
2025-04-21 21:50:26 -0400

    4.10 solution.

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -1,17 +1,40 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const assert = require('node:assert')
 const app = require('../app')
-
+const Blog = require('../models/blog')
 const api = supertest(app)
 
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blog = new Blog({
+        title: 'Test Blog',
+        author: 'Tester',
+        url: 'http://example.com',
+        likes: 5
+    })
+
+    await blog.save()
+})
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 })
 
+test('unique identifier is named id', async () => {
+    const blogs = await api.get('/api/blogs')
+
+    blogs.body.forEach(blog => {
+        assert.ok(blog.id) // id should be defined
+        assert.strictEqual(blog._id, undefined) // _id should not be present
+    })
+})
+
+
 after(async () => {
-  await mongoose.connection.close()
+    await mongoose.connection.close()
 })
